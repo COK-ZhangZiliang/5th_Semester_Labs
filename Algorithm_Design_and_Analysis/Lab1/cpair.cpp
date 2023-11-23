bf172c1398bbc62feef9ad53ab28ac6b42aeef54
Steve_ZL <1907973434@qq.com>
2023-11-23 22:41:44 +0800

    fix a fatal bug & add verify code

--- Algorithm_Design_and_Analysis/Lab1/cpair.cpp
+++ Algorithm_Design_and_Analysis/Lab1/cpair.cpp
@@ -1,4 +1,10 @@
-// fixme: bug
+/**
+ * @file cpair.cpp
+ * @author zhang ziliang (ziliangzhang@bupt.edu.cn)
+ * @brief 平面最近点对算法
+ * @version 0.1
+ * @date 2023-11-23
+ */
 #include <iostream>
 #include <cmath>
 #include <fstream>
@@ -9,7 +15,7 @@ using namespace std;
 const int INF = 0x3f3f3f3f;
 const int MAX_NUM = 2000; // 最大元素个数
 const double PI = 3.1415926535897932384626;
-const double R = 6371.0; // 地球半径
+const double R = 6378.137 * 1e3; // 地球半径
 
 typedef struct
 {
@@ -23,6 +29,7 @@ Point tmp[MAX_NUM];
 double distance(Point p1, Point p2);
 void merge(Point a[], Point b[], int l, int m, int r, int mode);
 void merge_sort(Point a[], int l, int r, int mode);
+bool cmp(Point p1, Point p2);
 void closest3(Point X[], int l, Point &p1, Point &p2, double &d);
 void closest(Point X[], Point Y[], Point Z[], int l, int r, Point &p1, Point &p2, double &d);
 
@@ -54,13 +61,6 @@ int main()
 
     file.close();
 
-    for (int i = 0; i <= row_idx - 1; i++)
-        cout << points[i].ENODEBID << endl;
-    for (int i = 0; i <= row_idx - 1; i++)
-        for (int j = i + 1; j <= row_idx - 1; j++)
-            if (points[i].ENODEBID == points[j].ENODEBID)
-                cout << "Duplicate ENODEBID: " << points[i].ENODEBID << endl;
-
     Point X[MAX_NUM], Y[MAX_NUM], Z[MAX_NUM];
     merge_sort(points, 0, row_idx - 1, 0); // 按照纬度排序
     for (int i = 0; i < row_idx; i++)
@@ -69,11 +69,55 @@ int main()
     for (int i = 0; i < row_idx; i++)
         Y[i] = points[i];
 
+    // 求解最近点对
     Point p1, p2;
     double d;
     closest(X, Y, Z, 0, row_idx - 1, p1, p2, d);
     cout << "The closest pair is: " << p1.ENODEBID << " " << p2.ENODEBID << endl;
-    cout << "The distance is: " << d << " km" << endl;
+    cout << "Point 1: " << p1.LATITUDE << " " << p1.LONGITUDE << endl;
+    cout << "Point 2: " << p2.LATITUDE << " " << p2.LONGITUDE << endl;
+    cout << "The distance is: " << d << " m" << endl;
+
+    // 求解次近点对
+    for (int i = 0; i < row_idx; i++)
+    {
+        if (p1.ENODEBID == X[i].ENODEBID)
+        {
+            X[i].LATITUDE = 0;
+            X[i].LONGITUDE = 0;
+        }
+        if (p1.ENODEBID == Y[i].ENODEBID)
+        {
+            Y[i].LATITUDE = 0;
+            Y[i].LONGITUDE = 0;
+        }
+    }
+    closest(X, Y, Z, 0, row_idx - 1, p1, p2, d);
+    cout << "The second closest pair is: " << p1.ENODEBID << " " << p2.ENODEBID << endl;
+    cout << "Point 1: " << p1.LATITUDE << " " << p1.LONGITUDE << endl;
+    cout << "Point 2: " << p2.LATITUDE << " " << p2.LONGITUDE << endl;
+    cout << "The distance is: " << d << " m" << endl;
+
+    // 验证结果
+    cout << "Verifying..." << endl;
+    double min_dist = INF;
+    for (int i = 0; i < row_idx; i++)
+        for (int j = i + 1; j < row_idx; j++)
+        {
+            double dist = distance(points[i], points[j]);
+            if (dist < min_dist && cmp(points[i], points[j]))
+                min_dist = dist;
+        }
+    cout << "The minimum distance is: " << min_dist << " m" << endl;
+    double min_dist2 = INF;
+    for (int i = 0; i < row_idx; i++)
+        for (int j = i + 1; j < row_idx; j++)
+        {
+            double dist = distance(points[i], points[j]);
+            if (dist < min_dist2 && cmp(points[i], points[j]) && dist != min_dist)
+                min_dist2 = dist;
+        }
+    cout << "The second minimum distance is: " << min_dist2 << " m" << endl;
 }
 
 // 计算两点之间的距离
@@ -114,12 +158,26 @@ void merge(Point a[], Point b[], int l, int m, int r, int mode)
         b[k++] = a[j++];
 }
 
+// 比较两个点位置是否相同
+bool cmp(Point p1, Point p2)
+{
+    if ((fabs(p1.LATITUDE - p2.LATITUDE) < 1e-8) && (fabs(p1.LONGITUDE - p2.LONGITUDE) < 1e-8))
+        return false;
+    return true;
+}
+
 // 求解三个点中的最近点对
 void closest3(Point X[], int l, Point &p1, Point &p2, double &d)
 {
     double d1 = distance(X[l], X[l + 1]);
     double d2 = distance(X[l], X[l + 2]);
     double d3 = distance(X[l + 1], X[l + 2]);
+    if (!cmp(X[l], X[l + 1]))
+        d1 = INF;
+    if (!cmp(X[l], X[l + 2]))
+        d2 = INF;
+    if (!cmp(X[l + 1], X[l + 2]))
+        d3 = INF;
     if (d1 <= d2 && d1 <= d3)
     {
         p1 = X[l];
@@ -168,6 +226,8 @@ void closest(Point X[], Point Y[], Point Z[], int l, int r, Point &p1, Point &p2
         p1 = X[l];
         p2 = X[r];
         d = distance(p1, p2);
+        if (!cmp(p1, p2))
+            d = INF;
         return;
     }
 
@@ -180,12 +240,25 @@ void closest(Point X[], Point Y[], Point Z[], int l, int r, Point &p1, Point &p2
 
     // 将Y[]以X[]的中位数为界分成两部分
     int m = (l + r) / 2;
-    int f = l, g = m + 1;
-    for (int i = l; i <= r; i++)
-        if (Y[i].LATITUDE > X[m].LATITUDE)
-            Z[g++] = Y[i];
-        else
-            Z[f++] = Y[i];
+    while (m < r && fabs(X[m].LATITUDE - X[m + 1].LATITUDE) < 1e-8)
+        m++;
+    if (m != r)
+    {
+        int f = l, g = m + 1;
+        for (int i = l; i <= r; i++)
+            if (Y[i].LATITUDE > X[m].LATITUDE)
+                Z[g++] = Y[i];
+            else
+                Z[f++] = Y[i];
+    }
+    else
+    {
+        m = (l + r) / 2;
+        for (int i = l; i <= r; i++)
+            Z[i] = X[i];
+        merge_sort(Z, l, m, 1);
+        merge_sort(Z, m + 1, r, 1);
+    }
 
     closest(X, Z, Y, l, m, p1, p2, d);
     double dr;    // 右部分的最近点对距离
@@ -211,6 +284,8 @@ void closest(Point X[], Point Y[], Point Z[], int l, int r, Point &p1, Point &p2
         for (int j = i + 1; j < k && Z[j].LONGITUDE - Z[i].LONGITUDE < d; j++)
         {
             double d1 = distance(Z[i], Z[j]);
+            if (!cmp(Z[i], Z[j]))
+                d1 = INF;
             if (d1 < d)
             {
                 d = d1;
